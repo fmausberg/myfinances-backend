abafbff71fb4cd32a1c25403bcfde5ac1826b7d8
fmausberg <felix.mausberg@gmail.com>
2025-02-12 15:59:36 +0100

    deleted myfinancesbackend.DataBaseService because of redundancies with authenticationframework

--- myfinances-backend/src/main/java/net/mausberg/myfinancesbackend/MyFinancesBackendApplication.java
+++ myfinances-backend/src/main/java/net/mausberg/myfinancesbackend/MyFinancesBackendApplication.java
@@ -3,9 +3,11 @@ package net.mausberg.myfinancesbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"net.mausberg.authentication_framework_backend", "net.mausberg.myfinancesbackend"})
 @EnableJpaRepositories(basePackages = "net.mausberg.myfinancesbackend.repository")
 @EntityScan(basePackages = "net.mausberg.myfinancesbackend.model")
 public class MyFinancesBackendApplication {
